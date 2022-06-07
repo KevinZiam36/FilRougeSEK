ee7a43b349be70fa0da3ce92d9ca988dfd549b50
ElodieOL <102534608+ElodieOL@users.noreply.github.com>
2022-06-07 13:13:24 +0200

    Update dataaccess.php

--- dataaccess/dataaccess.php
+++ dataaccess/dataaccess.php
@@ -3,6 +3,7 @@
 function ajouterEmailDatabase($pAdresseEmail)
 {
     try
+<<<<<<< Updated upstream
     {
         $sql = "INSERT INTO newsletter (email) value (?);";
         $db = new PDO('mysql:host=localhost;db=sek',
@@ -10,6 +11,11 @@ function ajouterEmailDatabase($pAdresseEmail)
     
     $stmt = $db->prepare($sql);
     $stmt->execute([$pAdresseEmail]);
+=======
+    { 
+    $db = new PDO('mysql:host=localhost;db=sek',
+    'root','');
+>>>>>>> Stashed changes
     }
 
     catch(PDOException $e)
